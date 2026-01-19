dc4aa415989ab0ee535f8ae9d744f5f88d654837
Muhammad Wajahat Tariq <wajahatariq4@gmail.com>
2026-01-19 22:28:42 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -483,10 +483,10 @@ async def get_manager_data(token: str):
                 docs.append(d)
             return docs
 
-        bill_data = clean_docs(billing_col.find().sort("created_at", -1).limit(200))
-        ins_data = clean_docs(insurance_col.find().sort("created_at", -1).limit(200))
-        design_data = clean_docs(design_col.find().sort("created_at", -1).limit(200))
-        ebook_data = clean_docs(ebook_col.find().sort("created_at", -1).limit(200))
+        bill_data = clean_docs(billing_col.find().sort("created_at", -1).limit(1000))
+        ins_data = clean_docs(insurance_col.find().sort("created_at", -1).limit(1000))
+        design_data = clean_docs(design_col.find().sort("created_at", -1).limit(1000))
+        ebook_data = clean_docs(ebook_col.find().sort("created_at", -1).limit(1000))
         
         stats_bill = calculate_mongo_stats(billing_col, 'billing')
         stats_ins = calculate_mongo_stats(insurance_col, 'insurance')
@@ -534,4 +534,5 @@ async def update_status(type: str = Form(...), id: str = Form(...), status: str 
         return {"status": "success", "message": "Updated in Database"}
     except Exception as e:
         return {"status": "error", "message": str(e)}
+
 
